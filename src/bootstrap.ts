06cffb30d95d803a68b5b3550b7bf15b8ff26a2f
quanggiaphthh <quanggiaphthh@gmail.com>
2026-09-24 11:15:14 +0700

    W4B: use packaged client module composition

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,18 +1,11 @@
 import { moduleRegistry } from './core/modules/moduleRegistry';
-import { homeManifest } from './modules/home/manifest';
-import { settingsManifest } from './modules/settings/manifest';
-import { tasksManifest } from './modules/tasks/manifest';
+import { registerPackagedClientModules } from './moduleComposition';
 import { auth } from './lib/firebase';
 
 export async function bootstrapClient() {
   console.log('[Client Bootstrap] Initializing module catalog...');
 
-  // Register Core modules
-  moduleRegistry.register(homeManifest);
-  moduleRegistry.register(settingsManifest);
-
-  // Register Domain modules
-  moduleRegistry.register(tasksManifest);
+  registerPackagedClientModules();
 
   // Phase 1: Wait for auth before syncing protected data
   await auth.authStateReady();
